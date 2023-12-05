c28fdd33cad4960e8aaed6c786fe95225d6d043d
jacqui <gitaujaquiline@gmail.com>
2023-12-05 21:13:24 +0300

    Modifying the UI

--- app/src/main/java/com/jacquigitau/bullseye/GameScreen.kt
+++ app/src/main/java/com/jacquigitau/bullseye/GameScreen.kt
@@ -1,13 +1,21 @@
 package com.jacquigitau.bullseye
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jacquigitau.bullseye.ui.theme.BullseyeTheme
 
 /**
@@ -20,26 +28,40 @@ import com.jacquigitau.bullseye.ui.theme.BullseyeTheme
 
 @Composable
 fun GameScreen() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "PUT THE BULLSEYE AS CLOSE AS YOU CAN")
-        Text(text = "89")
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "1")
-            Slider(
-                value = 0.5f,
-                valueRange = 0.01f..1f,
-                onValueChange = {}
-            )
-            Text(text = "100")
-        }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
 
-        Button(onClick = {  }) {
-            Text(text = "Hit Me")
+    ) {
+Spacer(modifier = Modifier.weight(.5f))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.weight(9f)
+        ) {
+            Text(text = "PUT THE BULLSEYE AS CLOSE AS YOU CAN")
+            Text(text = "89", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "1")
+                Slider(
+                    value = 0.5f,
+                    valueRange = 0.01f..1f,
+                    onValueChange = {}
+                )
+                Text(text = "100")
+            }
+
+            Button(onClick = { }) {
+                Text(text = "Hit Me")
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GameScreenPreview() {
     BullseyeTheme {
