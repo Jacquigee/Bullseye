634ca208d5b60c336c7a218f30f02152293353db
jacqui <gitaujaquiline@gmail.com>
2023-12-09 05:54:57 +0300

    Refactoring files into respective packages

--- app/src/main/java/com/jacquigitau/bullseye/MainActivity.kt
+++ app/src/main/java/com/jacquigitau/bullseye/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.jacquigitau.bullseye.screens.GameScreen
 import com.jacquigitau.bullseye.ui.theme.BullseyeTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/jacquigitau/bullseye/screens/GameScreen.kt
+++ app/src/main/java/com/jacquigitau/bullseye/screens/GameScreen.kt
@@ -1,4 +1,4 @@
-package com.jacquigitau.bullseye
+package com.jacquigitau.bullseye.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
@@ -16,8 +16,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jacquigitau.bullseye.R
+import com.jacquigitau.bullseye.components.GamePrompt
+import com.jacquigitau.bullseye.components.ResultDialog
 import com.jacquigitau.bullseye.ui.TargetSlider
 import com.jacquigitau.bullseye.ui.theme.BullseyeTheme
 import kotlin.math.abs
@@ -85,7 +89,13 @@ fun GameScreen() {
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.AUTOMOTIVE_1024p,
+    widthDp = 864,
+    heightDp = 432
+)
 @Composable
 fun GameScreenPreview() {
     BullseyeTheme {
