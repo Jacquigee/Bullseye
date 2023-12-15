9b4ef61a4e72db0cbb5b95295fb4c4486b7bb37b
jacqui <gitaujaquiline@gmail.com>
2023-12-15 14:57:54 +0300

    Modified the text style in the app

--- app/src/main/java/com/jacquigitau/bullseye/components/GamePrompt.kt
+++ app/src/main/java/com/jacquigitau/bullseye/components/GamePrompt.kt
@@ -2,6 +2,7 @@ package com.jacquigitau.bullseye.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,13 +28,17 @@ fun GamePrompt(targetValue: Int, modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
-        Text(text = stringResource(R.string.instruction_text))
+        Text(
+            text = stringResource(R.string.instruction_text),
+            style = MaterialTheme.typography.titleMedium.copy(letterSpacing = 1.sp, fontWeight = FontWeight.Bold)
+        )
         Text(
 //            text = "$targetValue",
             text = targetValue.toString(),
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp),
+            style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold)
+
         )
     }
 }
+
